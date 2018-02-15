a9175d4baa9fa98a611cf4579635e0c492eabcfe
Clement Leung <clhleung@ucsc.edu>
2018-02-15 15:27:45 -0800

    removed .class files

--- insight_testsuite/temp/run.sh
+++
@@ -1,6 +0,0 @@
-# run.sh
-# shell script to compile & run the Java programs that implement the required features
-
-
-javac ./src/Record.java ./src/Analyze.java
-java -cp ./src/ Analyze "itcont.txt" "percentile.txt"
